0d58b23aa5c3b6338e1cdd8144f8a47fc27a2bc8
Alex Bondar <zmagar@gmail.com>
2022-07-29 14:26:41 -0700

    getUser outside

---
+++ react-app/src/components/getUserInfo.js
@@ -0,0 +1,16 @@
+
+async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+
+  export default getUserInfo;
+
+
